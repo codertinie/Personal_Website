19f4491e700aeb9e8cc886b083fa03b2c2bf3e21
codertinie <martin.munyao@moringaschool.com>
2023-09-11 12:34:48 +0300

    Added an image and basic styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "../CSS/Home.css"
+import myImage from "../Assets/images/Martin_Munyao-removebg.png"
 
 const Home = () => {
   return ( 
-    <>
-      <h2>Home </h2>
-    </>
+    <div className="homepage-container">
+    <div className="hero-section">
+      <div className="hero-text">
+        <h1 className="hero-title">Hello, I'm Martin Munyao</h1>
+        <p className="hero-subtitle">Front-End Developer | Creative Thinker</p>
+        <button className="cta-button">Hire Me</button>
+      </div>
+      <div className="hero-image">
+        <img 
+        src={myImage} 
+        alt="MyImage" 
+        className="profile-image"
+        />
+      </div>
+    </div>
+  </div>
+    
    );
 }
  
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,34 @@
 import React from "react";
-import "../CSS/Navbar.css"
+import "../CSS/Navbar.css";
 
 const Navbar = () => {
-  return ( 
-    <nav>
-      <ul>
-        <li> <a href="#home">Home</a></li>
-        <li> <a href="#about">About</a></li>
-        <li> <a href="#contact">Contact</a></li>
-      </ul>
-    </nav>
-   );
-}
- 
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="logo">Your Logo or Text</div>
+      <div class="cta-button">
+        <a href="#contact">Get in Touch</a>
+      </div>
+    </header>
+  );
+};
+
 export default Navbar;
