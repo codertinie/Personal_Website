10d0fdbbe76c1048b9ba430e1ad549540955cb2e
codertinie <munyao25@gmail.com>
2025-07-06 14:44:46 +0300

    Modified the hero-section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,27 +1,26 @@
 import React from "react";
-import "../CSS/Home.css"
-import myImage from "../Assets/images/Martin_Munyao-removebg.png"
+import "../CSS/Home.css";
+import myImage from "../Assets/images/python.png";
 
 const Home = () => {
-  return ( 
+  return (
     <div className="homepage-container">
-    <div className="hero-section">
-      <div className="hero-text">
-        <h1 className="hero-title">Hello, I'm Martin Munyao</h1>
-        <p className="hero-subtitle">Front-End Developer | Creative Thinker</p>
-        <button className="cta-button">Hire Me</button>
-      </div>
-      <div className="hero-image">
-        <img 
-        src={myImage} 
-        alt="MyImage" 
-        className="profile-image"
-        />
+      <div className="hero-section">
+        <div className="hero-text">
+          <h1 className="hero-title">
+            Hello, I'm <span className="highlight">Martin Munyao</span>
+          </h1>
+          <p className="hero-subtitle">
+            Front-End Developer | Creative Thinker | React Enthusiast
+          </p>
+          <button className="cta-button">Hire Me</button>
+        </div>
+        <div className="hero-image">
+          <img src={myImage} alt="MyImage" className="profile-image" />
+        </div>
       </div>
     </div>
-  </div>
-    
-   );
-}
- 
+  );
+};
+
 export default Home;
