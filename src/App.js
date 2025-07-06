41d2579799f72ec932049e191845e7c4c703393f
codertinie <munyao25@gmail.com>
2025-07-06 14:46:07 +0300

    Included the new components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import About from './Components/About';
+import Blog from './Components/Blog';
 import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
+import Portfolio from './Components/Portfolio';
 
 function App() {
   return (
@@ -14,9 +17,16 @@ function App() {
       <section id='about'>
         <About />
       </section>
+      <section id='porfolio'>
+        <Portfolio />
+      </section>
+      <section id='blog'>
+        <Blog />
+      </section>
       <section id='contact'>
         <Contact />
       </section>
+      <Footer />
     </div>
   );
 }
